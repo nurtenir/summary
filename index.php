a08be680cc1c9bf3f9096e8c259cc3e3f1f91f7b
nurtenir <92497598+nurtenir@users.noreply.github.com>
2021-11-18 12:37:03 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nurtenir</title>
+</head>
+<body>
+    <h1>My summary</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Anarbek uulu Nurtenir</h1>
+</body>
+</html>
+
+
+</head>
+<body>
+	<h1>Handsome boy </h1>
+	 <img src="nuru.jpeg"> 
+	<a href="index.html">home</a>
+<body>
+</html>
+</head>
+<body>
+    <h1>23.06.2004</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My phone number;0502484857</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My adress: Aaly Tokombaeva 27</h1>
+</body>
+</html>
+
+
+</head>
+<body>
+    <h1>i am senior collage student at it bussines college in International Ala-Too university </h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>My department is Computer system and comploxes</h1>
+</body>
+</html>
+
+</head>
+<body>
+    <h1>Next year i am planning to apply MIT</h1>
+</body>
+</html>
